aa22de8843d00ccfb0b2ddc1b864614d8709986a
sori <sori.coralcat@developer.com>
2026-09-11 11:30:00 +0900

    Supabase 연동 및 WORKS 데이터 조회

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+
 function Works() {
+  const [works, setWorks] = useState([]);
+
+  useEffect(() => {
+    getWorks();
+  }, []);
+
+  async function getWorks() {
+    const { data, error } = await supabase.from("works").select("*");
+
+    console.log("data:", data);
+    console.log("error:", error);
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    setWorks(data);
+
+    return;
+  }
+
   return (
     <section>
-      <h2>Selected Works</h2>
-
-      <div>
-        <p>작품이 들어갈 자리</p>
-      </div>
+      {works.map((works) => (
+        <div key={works.id}>
+          <div className="title">
+            <h2>{works.title}</h2>
+            <small>{works.year}</small>
+            <div>{works.description}</div>
+          </div>
+        </div>
+      ))}
     </section>
   );
 }
